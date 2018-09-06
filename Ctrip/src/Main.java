338ea8efeb4a751cf9da994adb4fa90e83d7882c
Yanbo <yanbo_l@foxmail.com>
2018-09-06 11:45:33 +0800

    Add Ctrip

---
+++ Ctrip/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        long num = sn.nextLong();
+        int count = 0;
+        while (num > 0){
+            if ((num&1)==1){
+                count+=1;
+            }
+            num = num>>1;
+        }
+        System.out.println(count);
+    }
+}
